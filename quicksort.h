0a5ba8356140a499b29ceaa6576182db5318054d
ILMAESTRO3511 <151433954+ILMAESTRO3511@users.noreply.github.com>
2024-03-15 11:40:29 +0100

    Update

--- quicksort.h
+++ quicksort.h
@@ -1,29 +1,34 @@
 int partition(int arr[], int low, int high, int *calc) {
     int pivot = arr[high];
     int i = low - 1;
-    *calc += 3;
+    *calc += 4;
 
-    *calc += 1;
+    *calc += 1; //init j
     for (int j = low; j <= high - 1; j++) {
-		*calc += 2;
+		*calc+=1; //end for
 		
-        *calc += 1;
+        *calc += 1; //check if
         if (arr[j] <= pivot) {
             *calc += 2;
             i++;
             swap(&arr[i], &arr[j], calc);
         }
+        
+        *calc+=1; //increase j
+        *calc+=1; //go up
     }
-    *calc += 1;
 
-    *calc += 1;
-    swap(&arr[i + 1], &arr[high], calc);
+    swap(&arr[i + 1], &arr[high], calc); *calc += 1;
+    *calc += 1; //return
     return i + 1;
 }
 
 void quickSort(int arr[], int low, int high, int *calc) {
-    *calc += 1;
-    if(high <= low) return;
+    *calc += 1; // check if
+    if(high <= low) {
+    	*calc += 1; //return
+		return;
+	}
     
     int p = partition(arr, low, high, calc);
     quickSort(arr, low, p - 1, calc);
--- selectionSort.h
+++ selectionSort.h
@@ -2,19 +2,27 @@ void selectionSort(int arr[], int dim, int *operazioni) {
     int i, j, min_idx;
     *operazioni+=3;
 
-    *operazioni+=(2*(dim-1)+2);
+    *operazioni+=1; //init i
     for (i = 0; i < dim-1; i++) {
-        min_idx = i;
-        *operazioni+=1;
-        *operazioni+=(2*(dim)+2);
+    	*operazioni+=1; //end for
+    	
+        min_idx = i; *operazioni+=1;
+        
+        *operazioni+=1; //init j
         for (j = i+1; j < dim; j++){
+        	*operazioni+=1; //end for
+        	
         	*operazioni+=1;
           	if (arr[j] < arr[min_idx]){
-          		min_idx = j;
-          		*operazioni+=1;
-			  }
+          		min_idx = j; *operazioni+=1;
+			}
+			
+			*operazioni+=1; //increase j
+        	*operazioni+=1; //go up
 		}	
-        swap(&arr[min_idx], &arr[i], operazioni);
-        *operazioni+=1;
+        swap(&arr[min_idx], &arr[i], operazioni); *operazioni+=1;
+        
+        *operazioni+=1; //increase i
+        *operazioni+=1; //go up
     }
 }
